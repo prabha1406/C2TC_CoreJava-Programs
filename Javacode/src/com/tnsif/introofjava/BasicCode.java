58026d1511afd8a9da806ac4d08055495378cf01
kumar <kumar@Mohana-prabha>
2025-12-17 10:07:49 +0530

    Initial commit

---
+++ Javacode/src/com/tnsif/introofjava/BasicCode.java
@@ -0,0 +1,30 @@
+package com.tnsif.introofjava;
+
+public class BasicCode {
+
+	public static void main(String[] args) {
+		
+		int age = 21;
+		double salary = 50000.00;
+		boolean isPassed = true;
+		char grade = 'A';
+		
+		
+		double newSalary = age; //Implicit or internal conversion - Data widening
+		
+		int roundSalary = (int)salary; // Explicit or External - Data Narrowing
+		
+		//Arithmetic operation
+		int increment = 5000;
+		
+		int total = roundSalary + increment;
+		
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(roundSalary);
+		System.out.println(total);
+
+	}
+
+}
