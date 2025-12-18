764ad262de670b39d8ada49d96daeacfadbae58d
kumar <kumar@Mohana-prabha>
2025-12-18 22:14:05 +0530

    Initial commit

--- Javacode/src/com/tnsif/packagesandaccessmodifiers/PacakgeTest.java
+++ Javacode/src/com/tnsif/packagesandaccessmodifiers/PacakgeTest.java
@@ -1,5 +1,14 @@
 package com.tnsif.packagesandaccessmodifiers;
 
+import com.tnsif.packagetest.MainPackage;
+
 public class PacakgeTest {
+
+	public static void main(String[] args) {
+		
+		MainPackage mp = new MainPackage();
+		mp.display();
+
+	}
 
 }
--- Javacode/src/com/tnsif/polymorphism/AnimalClass.java
+++ Javacode/src/com/tnsif/polymorphism/AnimalClass.java
@@ -1,5 +1,31 @@
 package com.tnsif.polymorphism;
 
-public class AnimalClass {
+//same method with same paratemeters
+
+class AnimalClass{
+	void sound() {
+		System.out.println("Animals can make sound");
+	}
+}
+
+
+class Lion extends AnimalClass{
+	void sound() {
+		System.out.println("Lion roars......");
+	}
+}
+
+class MethodOverridingDemo {
+
+	public static void main(String[] args) {
+	
+		AnimalClass ac = new AnimalClass();
+		ac.sound();
+		
+		
+		/*
+		 * Lion l = new Lion(); l.sound();
+		 */
+	}
 
 }
