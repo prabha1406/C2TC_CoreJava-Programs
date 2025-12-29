15b5b758770fc964e58190611eced657d89f9c14
kumar <kumar@Mohana-prabha>
2025-12-29 22:24:12 +0530

    Initial commit

---
+++ Javacode/src/com/tnsif/exceptionhandling/WithoutException.java
@@ -0,0 +1,11 @@
+//Program without exception handling
+package com.tnsif.exceptionhandling;
+public class WithoutException {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+		System.out.println("the program continues....");
+	}
+	
+}
